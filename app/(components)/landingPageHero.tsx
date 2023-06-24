aebe88fd43f6efb7163cd956b8bf7a08209744ad
Jacob <39087671+jackobV@users.noreply.github.com>
2023-06-24 23:09:40 +0200

    f

--- app/(components)/landingPageHero.tsx
+++ app/(components)/landingPageHero.tsx
@@ -1,5 +1,6 @@
 import LandingPageHeroCallout from "@/app/(components)/landingPageHeroCallout";
-
+import classroomPhoto from "../(media)/test-classroom-pic.jpg"
+import Image from "next/image";
 export default function LandingPageHero(){
     return(
         <div className="px-4 pt-5">
@@ -7,6 +8,9 @@ export default function LandingPageHero(){
                 Test nanečisto, který tě <span className=" text-blue-600">hladce </span>dostane na vysněnou školu
             </h1>
             <p className="pt-4 text-gray-800 ">Jsme tým zkušených lektorů, který pořádá testy nanečisto. Naše testy slouží jako přesná simulace testů CERMAT. Zaměřujeme se na přípravu pro devátou třídu - přijímačky na střední školy a čtyřletá gymnázia</p>
+            <div className="pt-4">
+            <Image src={classroomPhoto} alt={"třída plná studentů"}  className="rounded-lg"/>
+            </div>
             <LandingPageHeroCallout />
         </div>
     )
