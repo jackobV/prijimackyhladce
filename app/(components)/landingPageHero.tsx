d21a0d52f22cf900480b3decb989a09e70e2d808
Jacob <39087671+jackobV@users.noreply.github.com>
2023-06-24 23:39:51 +0200

    f

--- app/(components)/landingPageHero.tsx
+++ app/(components)/landingPageHero.tsx
@@ -1,17 +1,24 @@
 import LandingPageHeroCallout from "@/app/(components)/landingPageHeroCallout";
-import classroomPhoto from "../(media)/test-classroom-pic.jpg"
+import classroomPhoto from "../(media)/classroompicture-copy.jpg"
 import Image from "next/image";
 export default function LandingPageHero(){
     return(
-        <div className="px-4 pt-5">
-            <h1 className="font-black tracking-tight text-2xl inline">
+        <div className="px-4 pt-5 flex flex-col md:flex-row max-w-5xl mx-auto gap-x-10">
+            <div className="md:max-w-md ">
+            <h1 className="md:leading-relaxed leading-relaxed font-black md:font-bold tracking-tight text-2xl md:text-3xl inline">
                 Test nanečisto, který tě <span className=" text-blue-600">hladce </span>dostane na vysněnou školu
             </h1>
-            <p className="pt-4 text-gray-800 ">Jsme tým zkušených lektorů, který pořádá testy nanečisto. Naše testy slouží jako přesná simulace testů CERMAT. Zaměřujeme se na přípravu pro devátou třídu - přijímačky na střední školy a čtyřletá gymnázia</p>
-            <div className="pt-4">
-            <Image src={classroomPhoto} alt={"třída plná studentů"}  className="rounded-lg"/>
+            <p className="pt-4 text-gray-800 md:leading-loose">Jsme tým zkušených lektorů, který pořádá testy nanečisto. Naše testy slouží jako přesná simulace testů CERMAT. Zaměřujeme se na přípravu pro devátou třídu - přijímačky na střední školy a čtyřletá gymnázia</p>
+            <div className="hidden md:inline">
+                <LandingPageHeroCallout />
             </div>
-            <LandingPageHeroCallout />
+            </div>
+            <div className="pt-4 md:pt-0 w-full h-84 items-center ">
+            <Image src={classroomPhoto} alt={"třída plná studentů"}  className="rounded-lg w-full h-full object-cover"/>
+            </div>
+            <div className="md:hidden">
+                <LandingPageHeroCallout />
+            </div>
         </div>
     )
 }
--- app/(components)/landingPageHeroCallout.tsx
+++ app/(components)/landingPageHeroCallout.tsx
@@ -1,6 +1,6 @@
 export default function LandingPageHeroCallout(){
     return(
-        <div className="pt-10">
+        <div className="pt-10 md:max-w-xs">
         <div className="bg-blue-500 py-2 px-2 rounded-xl flex flex-row justify-between">
             <p className="text-white font-medium ">
                 Předprodej 2023/24 zahájen!
