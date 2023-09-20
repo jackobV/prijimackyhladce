96732910d7543fd0ff37dd070b7029de1ce39917
Jacob <39087671+jackobV@users.noreply.github.com>
2023-09-20 16:36:20 +0200

    f

--- app/api/stripewebhook/route.ts
+++ app/api/stripewebhook/route.ts
@@ -1,23 +1,31 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {NextRequest, NextResponse} from "next/server";
 import Stripe from 'stripe'
+import Cors from "micro-cors";
+import { headers } from "next/headers";
 import PocketBase from "pocketbase";
 import {buffer} from "micro";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const cors = Cors({
+    allowMethods: ["POST", "HEAD"],
+});
 const webhookSecret:string = process.env.STRIPE_WEBHOOK_SERCET_KEY
-export async function POST(req: NextRequest, res: NextResponse){
-    const buf = await buffer(req)
 
-    const sig = req.headers["stripe-signature"]
-    let event: Stripe.Event
+
+export async function POST(req: Request){
     try {
-        event = stripe.webhooks.constructEvent(buf.toString(),sig,webhookSecret)
-    }catch (err){
-        console.log(err)
-        return
-    }
-    if (event.type === "checkout.session.completed"){
-        const session = event.data.object;
-        console.log(session)
+        const body = await req.text();
+        const signature = headers().get("stripe-signature");
+        const event = stripe.webhooks.constructEvent(body,signature,webhookSecret);
+        if (event.type === "checkout.session.completed"){
+            console.log(event.data.object);
+        }
+        return NextResponse.json({result:event,ok:true})
+        }catch (error){
+        console.log(error)
+        return NextResponse.json({
+            message:"something went wrong",
+            ok:false,
+        },)
     }
 }
