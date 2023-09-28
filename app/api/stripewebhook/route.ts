99ead656086af637b9310edf9cb1b5b66522ba99
Jacob <39087671+jackobV@users.noreply.github.com>
2023-09-28 16:10:48 +0200

    f

--- app/api/stripewebhook/route.ts
+++ app/api/stripewebhook/route.ts
@@ -6,6 +6,7 @@ import { headers } from "next/headers";
 import PocketBase from "pocketbase";
 import {buffer} from "micro";
 import sendgrid from "@sendgrid/mail";
+import sgMail from "@sendgrid/mail";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const cors = Cors({
     allowMethods: ["POST", "HEAD"],
@@ -19,6 +20,29 @@ export async function POST(req: Request){
         const signature = headers().get("stripe-signature");
         const event = stripe.webhooks.constructEvent(body,signature,webhookSecret);
         if (event.type === "checkout.session.completed"){
+            console.log("trying to send email")
+            const sgMail = require('@sendgrid/mail')
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+            const msg = {
+                to: 'info@na-zkousku.cz', // Change to your recipient
+                from: 'info@na-zkousku.cz', // Change to your verified sender
+                subject: 'Sending with SendGrid is Fun',
+                text: 'and easy to do anywhere, even with Node.js',
+                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+            }
+            console.log(msg)
+            try {
+                console.log("sending email")
+                await sgMail.send(msg)
+                console.log("sent")
+            } catch (e){
+                console.log("error")
+                console.log(e)
+            }
+
+
+
+
             console.log(event.data.object);
             const customerEmail: string = event.data.object.email
             const customerId: string = event.data.object.metadata.user_id
@@ -43,22 +67,6 @@ export async function POST(req: Request){
                 } catch (e) {
                     console.log(e)
                 }
-            }
-            console.log(senggridApiKey)
-            const sgMail = require('@sendgrid/mail')
-            sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-            const msg = {
-                to: 'info@na-zkousku.cz', // Change to your recipient
-                from: 'info@na-zkousku.cz', // Change to your verified sender
-                subject: 'Sending with SendGrid is Fun',
-                text: 'and easy to do anywhere, even with Node.js',
-                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-            }
-            try {
-                await sgMail.send(msg)
-                console.log("sent")
-            } catch (e){
-                console.log(e)
             }
         }
 
