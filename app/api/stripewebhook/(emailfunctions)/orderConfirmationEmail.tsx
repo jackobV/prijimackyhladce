8581f91843658de668fdbe4f3e085d0a91253911
Jacob <39087671+jackobV@users.noreply.github.com>
2023-09-29 13:01:16 +0200

    f

---
+++ app/api/stripewebhook/(emailfunctions)/orderConfirmationEmail.tsx
@@ -0,0 +1,55 @@
+import sgMail from "@sendgrid/mail";
+
+export interface EmailConfirmationData {
+    email:string;
+    name:string;
+    surname:string;
+    purchaseId:string;
+    itemDates:Array<string>;
+    ticketIds:Array<string>;
+
+    totalPrice:number;
+}
+
+export default async function OrderConfirmationEmail({ emailData } : {emailData:EmailConfirmationData}){
+        console.log("trying to send email")
+        const sgMail = require('@sendgrid/mail')
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: `${emailData.email}`, // Change to your recipient
+            from: 'info@na-zkousku.cz', // Change to your verified sender
+            subject: `🎉 Potvrzení objednávky - na-zkousku.cz`,
+            text: `Děkujeme za Vaši objednávku! Tento email slouží jako potvrzení Vaší objednávky ${emailData.purchaseId}.`,
+            html: `<div><p>Dobrý den ${emailData.name} ${emailData.surname} <br><br> Děkujeme za Vaši objednávku! Tento email slouží jako potvrzení Vaší objednávky ${emailData.purchaseId}.</p>
+                    <br>
+                    <p><strong>📦 Detaily Objednávky:</strong></p>
+                    <br>
+                    <ul>
+                        <li><strong>Produkt</strong> : Test na zkoušku</li>
+                        <li><strong>Množství</strong> : ${emailData.ticketIds.length}</li>
+                        <li><strong>Datum testu</strong> : ${emailData.itemDates.map((item:string,key)=>(<p>{item}, </p>))}</li>
+                        <li><strong>Způsob platby</strong> : Online platba</li>
+                    </ul>
+                    <br>
+                    <h2>Kontakt:</h2>
+                    <br>
+                    <p>
+                    Pokud máte jakékoliv otázky nebo potřebujete asistenci, neváhejte nás kontaktovat na info@na-zkousku.cz.
+                    Děkujeme za nákup u Na Zkoušku!
+                    <br>
+                    S pozdravem,
+                    <br>
+                    Tým Na Zkoušku
+                    </p>
+                    </div>`,
+        }
+        console.log(msg)
+        try {
+            console.log("sending email")
+            await sgMail.send(msg)
+            return true
+        } catch (e){
+            console.log("error")
+            return false
+        }
+}
--- app/api/stripewebhook/route.ts
+++ app/api/stripewebhook/route.ts
@@ -7,6 +7,9 @@ import PocketBase from "pocketbase";
 import {buffer} from "micro";
 import sendgrid from "@sendgrid/mail";
 import sgMail from "@sendgrid/mail";
+import OrderConfirmationEmail, {
+    EmailConfirmationData
+} from "@/app/api/stripewebhook/(emailfunctions)/orderConfirmationEmail";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const cors = Cors({
     allowMethods: ["POST", "HEAD"],
@@ -26,43 +29,26 @@ export async function POST(req: Request){
         const signature = headers().get("stripe-signature");
         const event = stripe.webhooks.constructEvent(body,signature,webhookSecret);
         if (event.type === "checkout.session.completed"){
-            console.log("trying to send email")
-            const sgMail = require('@sendgrid/mail')
-            sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-            const msg = {
-                to: 'info@na-zkousku.cz', // Change to your recipient
-                from: 'info@na-zkousku.cz', // Change to your verified sender
-                subject: 'Sending with SendGrid is Fun',
-                text: 'and easy to do anywhere, even with Node.js',
-                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-            }
-            console.log(msg)
-            try {
-                console.log("sending email")
-                await sgMail.send(msg)
-                console.log("sent")
-            } catch (e){
-                console.log("error")
-                console.log(e)
-            }
             console.log(event.data.object);
             const customerEmail: string = event.data.object.email
             const customerId: string = event.data.object.metadata.user_id
             const databaseIds:Array<string> = event.data.object.metadata.database_ids.split(",");
             console.log(databaseIds)
+            const user = await pb.collection("users").getOne(customerId)
+            const ticketIdArray:Array<string> = [""]
+            const ticketDateArray: Array<string> = [""]
+            ticketIdArray.push(user.tickets)
+
             for (const databaseId of databaseIds){
                 try{
                     const ticket = await pb.collection("ticket").create({
                         "user":customerId,
                         "testy":databaseId
                     })
-                    const userAlreadyHasTickets = await pb.collection("users").getOne(customerId)
-                    const newTicketArray = [...userAlreadyHasTickets.tickets, ticket.id]
-                    await pb.collection("users").update(customerId,{
-                        "tickets":newTicketArray
-                    })
-                    const testDateAlreadyHasTickets = await pb.collection("testy").getOne(databaseId)
-                    const newTestTicketArray = [...testDateAlreadyHasTickets.tickets, ticket.id]
+                    ticketIdArray.push(ticket.id)
+                    const testDate = await pb.collection("testy").getOne(databaseId)
+                    ticketDateArray.push(testDate.date)
+                    const newTestTicketArray = [...testDate.tickets, ticket.id]
                     await pb.collection("testy").update(databaseId,{
                         "tickets":newTestTicketArray
                     })
@@ -70,6 +56,21 @@ export async function POST(req: Request){
                     console.log(e)
                 }
             }
+            const purchase = await pb.collection("purchase").create({
+                "user":customerId,
+                "tickets":ticketIdArray,
+                "paymentIntent":event.data.object.payment_intent
+            })
+            const emailData:EmailConfirmationData = {
+                email:customerEmail,
+                name: user.name,
+                surname: user.surname,
+                purchaseId:purchase.id,
+                itemDates:ticketDateArray,
+                totalPrice:event.data.object.amount_total,
+                ticketIds:ticketIdArray
+            }
+            await OrderConfirmationEmail({emailData:emailData})
         }
 
         return NextResponse.json({result:event,ok:true})
