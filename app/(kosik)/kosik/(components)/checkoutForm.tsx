d487db8874b12a67b485946a88420939e0bfedf9
Jacob <39087671+jackobV@users.noreply.github.com>
2023-10-18 16:30:07 +0200

    production-ready

--- app/(kosik)/kosik/(components)/checkoutForm.tsx
+++ app/(kosik)/kosik/(components)/checkoutForm.tsx
@@ -36,7 +36,9 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
     const [userEmailFail, setUserEmailFail] = useState("")
     const [userPasswordFail, setUserPasswordFail] = useState("")
     const [loginFail, setLoginFail] = useState(false)
-
+    const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/;
+    const [isPassRegexValid, setIsPassRegexValid] = useState(true);
+    const [loadingPayment,setLoadingPayment] = useState(false)
     const handlePay = async () => {
         const inCartIds: Array<string> = items.testDates.flatMap(item =>
         Array(item.inCart).fill(item.id))
@@ -135,6 +137,7 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
     const handleSubmit = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
         if(pb.authStore.isValid){
+            setLoadingPayment(true);
             try {
                 await handlePay()
             } catch (e) {
@@ -144,6 +147,7 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
             if(newCustomer === true){
                 if(validateForm() === true){
                     try {
+                        setLoadingPayment(true);
                         const data = {
                             "email": email,
                             "emailVisibility": true,
@@ -156,20 +160,24 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
                         await pb.collection("users").authWithPassword(email,password);
                         await handlePay();
                     } catch (error) {
+                        setLoadingPayment(false)
                         console.log(error)
                         setEmailFail("K registraci použijte jinou emailovou adresu.")
                     }}else{
+                    setLoadingPayment(false)
                     setPasswordAgain("")
                 }
             }else{
                 if (validateLogin() === true){
                     try{
+                        setLoadingPayment(true);
                         await pb.collection('users').authWithPassword(
                             userEmail,
                             userPassword,
                         );
                         await handlePay()
                     }catch(error){
+                        setLoadingPayment(false)
                         setUserEmailFail("")
                         setUserPasswordFail("")
                         setLoginFail(true)
@@ -203,6 +211,13 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
             setPaymentMethodFail(false)
         }
     },[paymentMethod])
+
+    const handleInputChangePass = (e:any) => {
+        const value = e.target.value;
+        setPassword(value)
+        setIsPassRegexValid(passwordRegex.test(value));
+    };
+
     return(
         <div className="mx-auto max-w-6xl px-6 lg:px-8 overflow-hidden pt-16">
                 <form className="flex flex-col md:flex-row gap-y-10 gap-x-20" onSubmit={handleSubmit}>
@@ -255,9 +270,16 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
                                         </div>
                                         <div className="flex flex-col w-full gap-y-2">
                                             <p className="text-gray-500">Heslo</p>
-                                            <input type="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}" className="rounded-lg w-full py-3 px-2 focus:ring-blue-600 placeholder-gray-400 placeholder border-gray-400" placeholder="Heslo" onChange={e => setPassword(e.target.value)} />
+                                            <input type="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}" className="rounded-lg w-full py-3 px-2 focus:ring-blue-600 placeholder-gray-400 placeholder border-gray-400" placeholder="Heslo" onChange={handleInputChangePass} />
                                             {passwordFail === "" ? <p className="hidden"></p>:<p className="text-red-500">{passwordFail}</p>}
-
+                                            {!isPassRegexValid && <div className="list-disc">
+                                                <p className="text-sm">Heslo musí obsahovat:</p>
+                                                <ul className="px-4 list-disc text-sm">
+                                                    <li>8 znaků</li>
+                                                    <li>Velké písmeno</li>
+                                                    <li>Číslici</li>
+                                                </ul>
+                                            </div>}
                                         </div>
                                         <div className="flex flex-col w-full gap-y-2">
                                             <p className="text-gray-500">Heslo znovu</p>
@@ -345,7 +367,16 @@ export default function CheckoutForm({items,numberOfDates, location}:{items:Test
                             </div>
                         </div>
                         <div className="pt-5 flex flex-col w-full">
-                            <button className="bg-blue-600 py-3 rounded-xl text-white " type="submit" id="gotopayment">Přejít na platbu</button>
+                            {loadingPayment ?
+                                <div role="status" className="bg-blue-600 py-3 rounded-xl text-white flex flex-col items-center justify-center w-full">
+                                    <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                    </svg>
+                                    <span className="sr-only">Loading...</span>
+                                </div>:
+                                <button className="bg-blue-600 py-3 rounded-xl text-white " type="submit" id="gotopayment">Přejít na platbu</button>
+                            }
 
                         </div>
                     </div>
