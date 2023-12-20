ff2b4a72bfe85fceec2ec1b7af53f2e14f568dbc
Jakub Zaloha <zalohjak@fit.cvut.cz>
2023-12-20 13:51:54 +0100

    fix:auth infinite loop

--- app/(aplikace)/aplikace/uzivatel/page.tsx
+++ app/(aplikace)/aplikace/uzivatel/page.tsx
@@ -1,9 +1,19 @@
+"use client"
 import Osuform from "@/app/(aplikace)/aplikace/uzivatel/(components)/osuform";
 import AppMenuBar from "@/app/(aplikace)/aplikace/(components)/appMenuBar";
 import PocketBase from "pocketbase";
 import {redirect} from "next/navigation";
+import {useEffect, useState} from "react";
 
 export default function Page(){
+    const pb = new PocketBase('https://pocketbase-production-2a51.up.railway.app');
+    const [loading, setLoading] = useState(false)
+    useEffect(()=>{
+        if (!pb.authStore.isValid) {
+            console.log("user is not logged in ")
+            redirect("/neautorizovan")
+        }
+    },[])
     return(
         <div className="max-w-7xl pt-12 mx-auto px-6">
             <Osuform />
