4f8330f7e70cb7e37deee66616ec16757544db12
Jakub Zaloha <zalohjak@fit.cvut.cz>
2024-03-21 15:53:25 +0100

    ftr: more informative checkout

--- app/(general)/prijimackyzarok/(components-prijimackyzarok)/StarRating.tsx
+++ app/(general)/prijimackyzarok/(components-prijimackyzarok)/StarRating.tsx
@@ -9,7 +9,7 @@ function StarIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 export function StarRating({ rating = 5 }: { rating?: 1 | 2 | 3 | 4 | 5 }) {
     return (
         <div className="flex gap-1">
-            {[...Array(rating).keys()].map((index) => (
+            {Array.from(Array(rating).keys()).map((index) => (
                 <StarIcon key={index} className="h-5 w-5 fill-current" />
             ))}
         </div>
